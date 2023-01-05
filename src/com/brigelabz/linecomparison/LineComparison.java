51a1cc39824dcbfbd1f31ca6abc2d9b714d9f303
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-01-05 16:37:26 +0530

    welcome message added

---
+++ src/com/brigelabz/linecomparison/LineComparison.java
@@ -0,0 +1,7 @@
+package com.brigelabz.linecomparison;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparison Program");
+    }
+}
